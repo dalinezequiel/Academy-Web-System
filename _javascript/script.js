be2ed8c1f03cc11de14bbe278372f92fdef9bb6f
dalinezequiel <dalinezequiel44@gmail.com>
2022-04-05 01:35:25 -0700

    first log

---
+++ _javascript/script.js
@@ -0,0 +1,12 @@
+function entrar(){
+    let user, pass;
+    user = document.getElementById("user").value;
+    pass = document.getElementById("senha").value;
+    alert("conteudo");
+    /*if(user.length == 0)
+    {
+        window.alert("vazio");
+    }else{
+        window.alert("conteudo");
+    }*/
+}
