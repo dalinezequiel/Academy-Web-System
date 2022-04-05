cddf51f9f11a0a58806b80e76ed448111265ab7c
dalinezequiel <dalinezequiel44@gmail.com>
2022-04-05 03:44:59 -0700

    thirth log

--- _javascript/script.js
+++ _javascript/script.js
@@ -1,12 +1,28 @@
 function entrar(){
-    let user, pass;
+    let user, pass, element;
     user = document.getElementById("user").value;
     pass = document.getElementById("senha").value;
-    alert("conteudo");
-    /*if(user.length == 0)
+
+    element = document.getElementById("user");
+
+    if(user.length > 0)
     {
-        window.alert("vazio");
+        user_pass(user, pass);
+    }else
+    {
+        window.alert("The fields are empty. Check it!");
+    }
+}
+
+function user_pass(user, pass)
+{
+    let _user = "admin";
+    let _pass = "123";
+
+    if(_user == user && _pass == pass)
+    {
+        window.alert("Logado com sucesso!");
     }else{
-        window.alert("conteudo");
-    }*/
+        window.alert("Username or password is incorrect!");
+    }
 }
