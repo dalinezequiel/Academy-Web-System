4265575e980540315a1951fb6e06e84851c8c87c
dalinezequiel <dalinezequiel44@gmail.com>
2022-04-09 21:49:16 -0700

    dashboard page

--- javascript/script.js
+++ javascript/script.js
@@ -9,26 +9,26 @@ function access()
     pass = document.getElementById("senha").value;
 
     var element = document.getElementById("erro");
-    element.style.padding = "5px";
-
     var text;
     
     if(user.length > 0 && pass.length > 0)
     {
         if(_user == user && _pass == pass)
         {
-            element.style.backgroundColor = "rgb(19, 192, 19)";
+            window.location.href = "../page/dashboard.html";
+            element.style.color = "rgb(19, 192, 19)";
             text = "Access granted!";
             element.innerHTML = text;
+            window.location.href = "../page/dashboard.html";
         }else{
-            element.style.backgroundColor = "#fe8b8e";
-            text = "Username or password is incorrect!";
+            element.style.color = "#fe8b8e";
+            text = "Inavlid username and password!";
             element.innerHTML = text;
         }
         return true;
     }else
     {
-        element.style.backgroundColor = "#fe8b8e";
+        element.style.color = "#fe8b8e";
         text = "The fields are empty. Check it!";
         element.innerHTML = text;
         return false;
